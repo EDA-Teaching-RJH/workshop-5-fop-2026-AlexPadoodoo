c12f6b7b39af5095fe93dd0af2b78afac6359e74
AlexPadoodoo <app10@kent.ac.uk>
2026-02-02 12:35:38 +0000

    i've done the prompt to insert coin, the change bit and the check if user has enough money. I still need to do a while loop

--- coffee.py
+++ coffee.py
@@ -1,1 +1,18 @@
-#Your code goes here
+import math
+
+#prompts user to insert coin
+print("**DISCLAIMER**")
+print("This machine only accepts 50p, 20p, 10p, 5p ")
+
+insert_coin = int(input("Please insert a coin: "))
+print("Total cost: 75p")
+
+#program checks if user has enough money
+if insert_coin ==75:
+    print("Thank you for your service! ")
+
+# Calculates change required
+
+elif insert_coin >75:
+    change = insert_coin - 75
+    print(f"Your change is: ", + change)
