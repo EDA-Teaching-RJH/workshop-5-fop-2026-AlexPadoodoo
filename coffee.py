3c808f04da8c9136d3cb547033ad69f85fecfec3
AlexPadoodoo <app10@kent.ac.uk>
2026-02-02 13:07:23 +0000

    finishing touches

--- coffee.py
+++ coffee.py
@@ -5,14 +5,14 @@ print("**DISCLAIMER**")
 print("**This machine only accepts 50p, 20p, 10p, 5p**")
 
 print("Total Cost: 75p")
-amount_due = 75
 
 #calculates the amount due
+amount_due = 75
 while amount_due > 0:
     print(f"Amount Due: {amount_due}p")
     insert_coin = int(input("Please insert a coin: "))
 
-#program checks if user has enough money
+#program checks if user has input the correct currency amount
     if insert_coin in [50, 20, 10, 5]:
         amount_due -= insert_coin
 
@@ -22,4 +22,5 @@ while amount_due > 0:
 # Calculates change required
 
 change = abs(amount_due)
+print("** Transaction Complete! **")
 print(f"Your change is: {change}p")
