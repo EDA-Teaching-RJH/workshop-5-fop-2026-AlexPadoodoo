3cf5f350383eff679fa31b70a3eabbc276aedc7d
AlexPadoodoo <app10@kent.ac.uk>
2026-02-02 12:39:49 +0000

    Make working code

--- coffee.py
+++ coffee.py
@@ -2,7 +2,7 @@ import math
 
 #prompts user to insert coin
 print("**DISCLAIMER**")
-print("This machine only accepts 50p, 20p, 10p, 5p ")
+print("**This machine only accepts 50p, 20p, 10p, 5p**")
 
 insert_coin = int(input("Please insert a coin: "))
 print("Total cost: 75p")
@@ -15,4 +15,4 @@ if insert_coin ==75:
 
 elif insert_coin >75:
     change = insert_coin - 75
-    print(f"Your change is: ", + change)
+    print(f"Your change is: {change}p")
